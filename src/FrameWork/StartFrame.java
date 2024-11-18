e4f4b6ce0dc60451d1bd5a2b5de9c3be8eecf28b
995929122 <995929122@qq.com>
2024-11-19 01:01:23 +0800

    11.18

--- src/FrameWork/StartFrame.java
+++ src/FrameWork/StartFrame.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionListener;
 import java.io.*;
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import FrameWork.*;
 
 
 
@@ -28,14 +27,36 @@ public class StartFrame extends JFrame implements ActionListener {
         this.setVisible(true);
     }
 	
-    Label label = new Label("Welcome to Start Panel");
+    
     Button backButton=new Button("Back");
+
+    Button Mode1=new Button("Mode1");
+    Button Mode2=new Button("Mode2");
+    
+
+
+    Box ModeBox=Box.createVerticalBox();
+    
+    //实例化面板
     public StartFrame(String imagePath) {
 		super("记个词先");
 		FrameBackgroundInit(imagePath);
 		this.setLayout(new BorderLayout());
 		
-		this.add(label, BorderLayout.NORTH);
+        ModeBox.add(Box.createHorizontalStrut(250));
+        //添加按钮并设置按钮字体
+        ModeBox.add(Box.createVerticalStrut(350));
+		Mode1.setFont(new Font("Serif", Font.BOLD | Font.ITALIC, 66));ModeBox.add(Mode1);
+		ModeBox.add(Box.createVerticalStrut(50));
+        Mode2.setFont(new Font("Serif", Font.BOLD | Font.ITALIC, 66));ModeBox.add(Mode2);
+		
+		this.add(ModeBox,BorderLayout.NORTH);
+		
+ 
+		Mode1.addActionListener(this);
+		Mode2.addActionListener(this);
+		
+        
 		
 		//将按钮放在面板右边 将面板放在窗口下方 完美
 		JPanel buttonPanel = new JPanel();
@@ -52,5 +73,27 @@ public class StartFrame extends JFrame implements ActionListener {
             this.dispose();
             new MainFrame("D:\\Java_eclipse_workspace\\Practical_Work\\src\\FrameWork\\image.png");
         }
+        if(e.getSource()==Mode1) {
+        	System.out.println("Mode1 按钮被点击！");
+        	this.remove(ModeBox); // 移除 ModeBox 面板
+            this.revalidate();    // 通知布局管理器重新布局
+            this.repaint();       // 重新绘制窗口
+            // 在这里添加 Mode1 按钮的逻辑
+        }
+        if(e.getSource()==Mode2) {
+        	System.out.println("Mode2 按钮被点击！");
+        	this.remove(ModeBox); // 移除 ModeBox 面板
+            this.revalidate();    // 通知布局管理器重新布局
+            this.repaint();       // 重新绘制窗口
+            // 在这里添加 Mode1 按钮的逻辑
+        }
+        
     }
 }
+
+
+class Mode1_Panel extends JPanel{
+        
+
+}
+
